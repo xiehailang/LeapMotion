d3576d858267060452b8a4bc3aa6262ab5c5ef0a
xiehailang <896474251@qq.com>
2018-08-02 19:31:14 +0800

    使用LeapMotion

---
+++ src/LeapMotion/LeapMotionDemo.java
@@ -0,0 +1,178 @@
+package LeapMotion;
+
+import java.io.IOException;
+import java.lang.Math;
+import com.leapmotion.leap.*;
+import com.leapmotion.leap.Gesture.State;
+
+class SampleListener extends Listener {
+    public void onInit(Controller controller) {
+        System.out.println("Initialized");
+    }
+
+    public void onConnect(Controller controller) {
+    	// 当Controller对象成功连接到Leap Motion进程并插入Leap Motion硬件时，
+    	// 该Controller对象将其isConnected（）属性更改为true并调用Listener.onConnect（）回调。
+    	System.out.println("Connected");
+    	// 当控制器连接时，可以使用Controller.enableGesture（）和Controller.setPolicy（）方法
+    	// 设置控制器属性。例如，您可以使用以下onConnect()功能启用滑动手势：
+        controller.enableGesture(Gesture.Type.TYPE_SWIPE);
+        controller.enableGesture(Gesture.Type.TYPE_CIRCLE);
+        controller.enableGesture(Gesture.Type.TYPE_SCREEN_TAP);
+        controller.enableGesture(Gesture.Type.TYPE_KEY_TAP);// enableGesture() 启用或禁用指定手势类型的报告。默认情况下，所有手势类型都被禁用。
+    }
+
+    public void onDisconnect(Controller controller) {
+        //Note: not dispatched when running in a debugger.
+        System.out.println("Disconnected");
+    }
+
+    public void onExit(Controller controller) {
+        System.out.println("Exited");
+    }
+
+    // Leap Motion系统中的所有跟踪数据都通过Frame对象返回。
+    public void onFrame(Controller controller) {
+        // controller.frame() 连接后从控制器中获取Frame对象
+        Frame frame = controller.frame();
+        
+        System.out.println("Frame id: " + frame.id()
+                         + ", timestamp: " + frame.timestamp()
+                         + ", hands: " + frame.hands().count()
+                         + ", fingers: " + frame.fingers().count()
+                         + ", tools: " + frame.tools().count()
+                         + ", gestures " + frame.gestures().count());
+
+        //Get hands
+        for(Hand hand : frame.hands()) {
+            String handType = hand.isLeft() ? "Left hand" : "Right hand";
+            System.out.println("  " + handType + ", id: " + hand.id()
+                             + ", palm position: " + hand.palmPosition());
+
+            // Get the hand's normal vector and direction
+            Vector normal = hand.palmNormal();
+            Vector direction = hand.direction();
+
+            // Calculate the hand's pitch, roll, and yaw angles
+            System.out.println("  pitch: " + Math.toDegrees(direction.pitch()) + " degrees, "
+                             + "roll: " + Math.toDegrees(normal.roll()) + " degrees, "
+                             + "yaw: " + Math.toDegrees(direction.yaw()) + " degrees");
+
+            // Get arm bone
+            Arm arm = hand.arm();
+            System.out.println("  Arm direction: " + arm.direction()
+                             + ", wrist position: " + arm.wristPosition()
+                             + ", elbow position: " + arm.elbowPosition());
+
+            // Get fingers
+            for (Finger finger : hand.fingers()) {
+                System.out.println("    " + finger.type() + ", id: " + finger.id()
+                                 + ", length: " + finger.length()
+                                 + "mm, width: " + finger.width() + "mm");
+
+                //Get Bones
+                for(Bone.Type boneType : Bone.Type.values()) {
+                    Bone bone = finger.bone(boneType);
+                    System.out.println("      " + bone.type()
+                                     + " bone, start: " + bone.prevJoint()
+                                     + ", end: " + bone.nextJoint()
+                                     + ", direction: " + bone.direction());
+                }
+            }
+        }
+
+        // Get tools
+        for(Tool tool : frame.tools()) {
+            System.out.println("  Tool id: " + tool.id()
+                             + ", position: " + tool.tipPosition()
+                             + ", direction: " + tool.direction());
+        }
+
+        GestureList gestures = frame.gestures();
+        for (int i = 0; i < gestures.count(); i++) {
+            Gesture gesture = gestures.get(i);
+
+            switch (gesture.type()) {
+                case TYPE_CIRCLE:
+                    CircleGesture circle = new CircleGesture(gesture);
+
+                    // Calculate clock direction using the angle between circle normal and pointable
+                    String clockwiseness;
+                    if (circle.pointable().direction().angleTo(circle.normal()) <= Math.PI/2) {
+                        // Clockwise if angle is less than 90 degrees
+                        clockwiseness = "clockwise";
+                    } else {
+                        clockwiseness = "counterclockwise";
+                    }
+
+                    // Calculate angle swept since last frame
+                    double sweptAngle = 0;
+                    if (circle.state() != State.STATE_START) {
+                        CircleGesture previousUpdate = new CircleGesture(controller.frame(1).gesture(circle.id()));
+                        sweptAngle = (circle.progress() - previousUpdate.progress()) * 2 * Math.PI;
+                    }
+
+                    System.out.println("  Circle id: " + circle.id()
+                               + ", " + circle.state()
+                               + ", progress: " + circle.progress()
+                               + ", radius: " + circle.radius()
+                               + ", angle: " + Math.toDegrees(sweptAngle)
+                               + ", " + clockwiseness);
+                    break;
+                case TYPE_SWIPE:
+                    SwipeGesture swipe = new SwipeGesture(gesture);
+                    System.out.println("  Swipe id: " + swipe.id()
+                               + ", " + swipe.state()
+                               + ", position: " + swipe.position()
+                               + ", direction: " + swipe.direction()
+                               + ", speed: " + swipe.speed());
+                    break;
+                case TYPE_SCREEN_TAP:
+                    ScreenTapGesture screenTap = new ScreenTapGesture(gesture);
+                    System.out.println("  Screen Tap id: " + screenTap.id()
+                               + ", " + screenTap.state()
+                               + ", position: " + screenTap.position()
+                               + ", direction: " + screenTap.direction());
+                    break;
+                case TYPE_KEY_TAP:
+                    KeyTapGesture keyTap = new KeyTapGesture(gesture);
+                    System.out.println("  Key Tap id: " + keyTap.id()
+                               + ", " + keyTap.state()
+                               + ", position: " + keyTap.position()
+                               + ", direction: " + keyTap.direction());
+                    break;
+                default:
+                    System.out.println("Unknown gesture type.");
+                    break;
+            }
+        }
+
+        if (!frame.hands().isEmpty() || !gestures.isEmpty()) {
+            System.out.println();
+        }
+    }
+}
+
+class LeapMotionDemo {
+    public static void main(String[] args) {
+    	// Listener 对象提供了一个基于事件的机制来响应重要的Controller状态更改
+        SampleListener listener = new SampleListener();
+        // Controller对象,它会自动连接到Leap Motion服务，一旦建立连接，就可以使用Controller.frame（）方法从它获取跟踪数据,连接过程是异步的
+        Controller controller = new Controller();
+
+        // 添加一个侦听器
+        controller.addListener(listener);
+
+        // 保持此过程运行，直到按下Enter
+        System.out.println("Press Enter to quit...");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // 程序结束移除侦听器
+        controller.removeListener(listener);
+    }
+}
+
